13b407934488a00aa5464581af5ec59d4be2b4c0
Jack O'Connor <oconnor663@gmail.com>
2019-08-16 13:51:28 -0400

    new version using the nursery

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,21 @@
 
 use futures::channel::mpsc;
 use futures::channel::oneshot;
-use futures::future::{self, Either};
+use futures::select;
+use futures::pin_mut;
 use futures::prelude::*;
-use futures::stream::FuturesUnordered;
+use futures::stream::{FuturesUnordered, Fuse};
 use rand::prelude::*;
-use std::time::{Duration, Instant};
 use std::pin::Pin;
-use std::task::{Poll, Context};
+use std::task::{Context, Poll};
+use std::time::{Duration, Instant};
+use tokio::prelude::*;
 
-const NEXT_ATTEMPT_WAIT_MILLIS: u64 = 250;
+const WAIT_MILLIS: u64 = 250;
 const MAX_ATTEMPTS: u64 = 3;
 
 struct Nursery<T: Future> {
-    receiver: mpsc::Receiver<T>,
+    receiver: Fuse<mpsc::Receiver<T>>,
     futures: FuturesUnordered<T>,
 }
 
@@ -23,7 +25,7 @@ impl<T: Future> Nursery<T> {
         let (sender, receiver) = mpsc::channel(0);
         (
             Self {
-                receiver,
+                receiver: receiver.fuse(),
                 futures: FuturesUnordered::new(),
             },
             sender,
@@ -39,29 +41,22 @@ impl<T: Future> Stream for Nursery<T> {
         // the FuturesUnordered collection. Note that if we didn't loop until
         // Poll::Pending here, we wouldn't be guaranteed to get another wakeup
         // in the future.
-        let mut channel_open = true;
         loop {
             match Pin::new(&mut self.receiver).poll_next(cx) {
                 Poll::Ready(Some(future)) => self.futures.push(future),
-                Poll::Ready(None) => {
-                    channel_open = false;
-                    break;
-                }
-                Poll::Pending => break,
+                Poll::Ready(None) | Poll::Pending => break,
             }
         }
-        // If any futures are ready, return one item to the caller. Otherwise
-        // this poll_next() call might return Pending if there are unfinished
-        // futures, or Ready(None) if we don't have any futures right now.
+        // If any futures are ready, return one item to the caller.
         if let Poll::Ready(Some(item)) = Pin::new(&mut self.futures).poll_next(cx) {
             return Poll::Ready(Some(item));
         }
-        // If we didn't get an item above, but the channel is still open,
-        // return Pending to the caller. Otherwise we're done.
-        if channel_open {
-            Poll::Pending
-        } else {
+        // If there are no futures in the collection, and the channel is
+        // closed, we're done.
+        if self.receiver.is_done() && self.futures.is_empty() {
             Poll::Ready(None)
+        } else {
+            Poll::Pending
         }
     }
 }
@@ -82,71 +77,69 @@ async fn dummy_dns_lookup() -> Result<(), ()> {
 }
 
 fn log(id: u64, verb: &str, t0: Instant) {
-    eprintln!(
-        "{} {} at {} ms",
-        id,
-        verb,
-        (Instant::now() - t0).as_millis()
-    );
+    let millis = (Instant::now() - t0).as_millis();
+    eprintln!("{} {} at {} ms", id, verb, millis);
+}
+
+async fn happy_eyeballs() -> Result<(), ()> {
+    let t0 = Instant::now();
+    let (mut nursery, mut attempt_sender) = Nursery::new();
+
+    let attempt_starter = async {
+        let mut id = 0;
+        loop {
+            log(id, "started", t0);
+            let (fail_sender, fail_receiver) = oneshot::channel::<()>();
+            let _ = attempt_sender
+                .send(async move {
+                    match dummy_dns_lookup().await {
+                        Ok(()) => Ok(id),
+                        Err(()) => {
+                            let _ = fail_sender.send(());
+                            Err(id)
+                        }
+                    }
+                })
+                .await;
+            id += 1;
+            if id == MAX_ATTEMPTS {
+                eprintln!("Max attempts reached.");
+                drop(attempt_sender);
+                break;
+            }
+            let _ = fail_receiver
+                .timeout(Duration::from_millis(WAIT_MILLIS))
+                .await;
+        }
+    }.fuse();
+    pin_mut!(attempt_starter);
+
+    let attempt_waiter = async {
+        loop {
+            match nursery.next().await {
+                Some(Ok(id)) => {
+                    log(id, "succeeded", t0);
+                    return Ok(());
+                }
+                Some(Err(id)) => {
+                    log(id, "failed", t0);
+                }
+                None => {
+                    eprintln!("All attempts failed.");
+                    return Err(());
+                }
+            }
+        }
+    }.fuse();
+    pin_mut!(attempt_waiter);
+
+    select! {
+        _ = attempt_starter => attempt_waiter.await,
+        result = attempt_waiter => result,
+    }
 }
 
 #[tokio::main]
 async fn main() {
-    let t0 = Instant::now();
-    let mut unordered = FuturesUnordered::new();
-    let mut id = 0;
-    // Until we hit the maximum number of attempts, spawn an attempt each time
-    // through this loop.
-    while id < MAX_ATTEMPTS {
-        log(id, "started", t0);
-        let (fail_sender, fail_receiver) = oneshot::channel::<()>();
-        let timeout = tokio_timer::sleep(Duration::from_millis(NEXT_ATTEMPT_WAIT_MILLIS));
-        let mut next_attempt = future::select(fail_receiver, timeout);
-        unordered.push(async move {
-            match dummy_dns_lookup().await {
-                Ok(()) => {
-                    log(id, "succeeded", t0);
-                    Ok(())
-                }
-                Err(()) => {
-                    log(id, "faild", t0);
-                    let _ = fail_sender.send(());
-                    Err(())
-                }
-            }
-        });
-        id += 1;
-        // Keep waiting until it's time to fire the next attempt.
-        loop {
-            match future::select(unordered.next(), &mut next_attempt).await {
-                Either::Left((Some(Ok(())), _)) => {
-                    // A request succeeded. We're done!
-                    return;
-                }
-                Either::Left((Some(Err(())), _)) => {
-                    // A request failed. Just keep waiting.
-                }
-                Either::Left((None, _)) | Either::Right(_) => {
-                    // Either the most recent task failed or the timeout fired.
-                    // Time to kick off the next attempt! Break out of this
-                    // inner loop.
-                    break;
-                }
-            }
-        }
-    }
-    println!("Max attempts reached.");
-    // Now as long as there are tasks in flight, keep awaiting them.
-    while let Some(result) = unordered.next().await {
-        match result {
-            Ok(()) => {
-                // A request succeeded. We're done!
-                return;
-            }
-            Err(()) => {
-                // A request failed. Just keep waiting.
-            }
-        }
-    }
-    println!("Finished without success.");
+    let _ = happy_eyeballs().await;
 }
