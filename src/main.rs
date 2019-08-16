7fbb41cabd8424c4d11284f531b68d390eb6fc20
Jack O'Connor <oconnor663@gmail.com>
2019-08-16 12:39:00 -0400

    a bit of cleanup

--- src/main.rs
+++ src/main.rs
@@ -8,55 +8,52 @@ use rand::prelude::*;
 use std::time::{Duration, Instant};
 
 const NEXT_ATTEMPT_WAIT_MILLIS: u64 = 250;
-const MAX_ATTEMPTS: usize = 3;
-
-fn t_millis(start: Instant) -> u128 {
-    (Instant::now() - start).as_millis()
-}
+const MAX_ATTEMPTS: u64 = 3;
 
 // Simulate a "DNS lookup" (really any kind of network IO, who cares what) by
 // just waiting for some random number of milliseconds, flipping a coin, and
 // then returning either success or failure.
-async fn dummy_dns_lookup(id: usize, start: Instant) -> Result<(), ()> {
+async fn dummy_dns_lookup() -> Result<(), ()> {
     let mut rng = rand::thread_rng();
     let sleep_millis = rng.gen_range(0, 1000);
     tokio_timer::sleep(Duration::from_millis(sleep_millis)).await;
     let succeeds: bool = rng.gen();
     if succeeds {
-        println!("{} succeeded at {}ms", id, t_millis(start));
         Ok(())
     } else {
-        println!("{} failed at {}ms", id, t_millis(start));
         Err(())
     }
 }
 
-async fn attempt_dns(
-    id: usize,
-    start: Instant,
-    fail_sender: oneshot::Sender<()>,
-) -> Result<(), ()> {
-    let result = dummy_dns_lookup(id, start).await;
-    if result.is_err() {
-        // We don't care if the receiver is gone.
-        let _ = fail_sender.send(());
-    }
-    result
+fn log(id: u64, verb: &str, t0: Instant) {
+    eprintln!("{} {} at {} ms", id, verb, (Instant::now() - t0).as_millis());
 }
 
 #[tokio::main]
 async fn main() {
-    let start = Instant::now();
+    let t0 = Instant::now();
     let mut unordered = FuturesUnordered::new();
     let mut id = 0;
     // Until we hit the maximum number of attempts, spawn an attempt each time
     // through this loop.
     while id < MAX_ATTEMPTS {
+        log(id, "started", t0);
         let (fail_sender, fail_receiver) = oneshot::channel::<()>();
         let timeout = tokio_timer::sleep(Duration::from_millis(NEXT_ATTEMPT_WAIT_MILLIS));
         let mut next_attempt = future::select(fail_receiver, timeout);
-        unordered.push(attempt_dns(id, start, fail_sender));
-        println!("began attempt {} at {}ms", id, t_millis(start));
+        unordered.push(async move {
+            match dummy_dns_lookup().await {
+                Ok(()) => {
+                    log(id, "succeeded", t0);
+                    Ok(())
+                }
+                Err(()) => {
+                    log(id, "faild", t0);
+                    let _ = fail_sender.send(());
+                    Err(())
+                }
+            }
+        });
         id += 1;
         // Keep waiting until it's time to fire the next attempt.
         loop {
