6985839eec59751611b219ef2df9c3dccc477f7b
Jack O'Connor <oconnor663@gmail.com>
2019-08-21 01:45:14 -0400

    switch to async_std and a simpler loop

--- src/main.rs
+++ src/main.rs
@@ -1,146 +1,118 @@
-#![feature(async_await)]
-
-use futures::channel::mpsc;
-use futures::channel::oneshot;
+use async_std::io;
+use async_std::task;
+use futures::pin_mut;
 use futures::prelude::*;
-use futures::stream::{Fuse, FuturesUnordered};
+use futures::select;
+use futures::stream::FuturesUnordered;
 use rand::prelude::*;
-use std::pin::Pin;
-use std::task::{Context, Poll};
+use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4};
 use std::time::{Duration, Instant};
-use tokio::prelude::*;
+use lazy_static::lazy_static;
 
 const WAIT_MILLIS: u64 = 250;
-const MAX_ATTEMPTS: u64 = 3;
 
-struct Nursery<T: Future> {
-    receiver: Fuse<mpsc::UnboundedReceiver<T>>,
-    futures: FuturesUnordered<T>,
+type Connection = ();
+
+lazy_static! {
+    static ref START: Instant = Instant::now();
 }
 
-impl<T: Future> Nursery<T> {
-    fn new() -> (Self, mpsc::UnboundedSender<T>) {
-        let (sender, receiver) = mpsc::unbounded();
-        (
-            Self {
-                receiver: receiver.fuse(),
-                futures: FuturesUnordered::new(),
-            },
-            sender,
-        )
-    }
-}
-
-impl<T: Future> Stream for Nursery<T> {
-    type Item = T::Output;
-
-    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<Self::Item>> {
-        // Take as many futures as we can from the receiver and seat them in
-        // the FuturesUnordered collection. Note that if we didn't loop until
-        // Poll::Pending here, we wouldn't be guaranteed to get another wakeup
-        // in the future.
-        loop {
-            match self.receiver.poll_next_unpin(cx) {
-                Poll::Ready(Some(future)) => self.futures.push(future),
-                Poll::Ready(None) | Poll::Pending => break,
-            }
-        }
-        // If any futures are ready, return one item to the caller.
-        if let Poll::Ready(Some(item)) = self.futures.poll_next_unpin(cx) {
-            return Poll::Ready(Some(item));
-        }
-        // If there are no futures in the collection, and the channel is
-        // closed, we're done.
-        if self.receiver.is_done() && self.futures.is_empty() {
-            Poll::Ready(None)
-        } else {
-            Poll::Pending
-        }
-    }
+fn log(addr: std::net::SocketAddr, verb: &str) {
+    let millis = (Instant::now() - *START).as_millis();
+    let last_digit = match addr.ip() {
+        std::net::IpAddr::V4(ip) => *ip.octets().last().unwrap(),
+        std::net::IpAddr::V6(ip) => *ip.octets().last().unwrap(),
+    };
+    eprintln!("{} {} at {} ms", last_digit, verb, millis);
 }
 
 // Simulate a "DNS lookup" (really any kind of network IO, who cares what) by
 // just waiting for some random number of milliseconds, flipping a coin, and
 // then returning either success or failure.
-async fn dummy_dns_lookup() -> Result<(), ()> {
-    let mut rng = rand::thread_rng();
-    let sleep_millis = rng.gen_range(0, 1000);
-    tokio_timer::sleep(Duration::from_millis(sleep_millis)).await;
-    let succeeds: bool = rng.gen();
+async fn dummy_connect(addr: std::net::SocketAddr) -> io::Result<Connection> {
+    log(addr, "started");
+    let sleep_millis = rand::thread_rng().gen_range(0, 1000);
+    task::sleep(Duration::from_millis(sleep_millis)).await;
+    let succeeds: bool = rand::thread_rng().gen();
     if succeeds {
+        log(addr, "succeeded");
         Ok(())
     } else {
-        Err(())
+        log(addr, "failed");
+        Err(io::Error::new(
+            io::ErrorKind::ConnectionAborted,
+            "connection failed",
+        ))
     }
 }
 
-fn log(id: u64, verb: &str, t0: Instant) {
-    let millis = (Instant::now() - t0).as_millis();
-    eprintln!("{} {} at {} ms", id, verb, millis);
-}
+async fn happy_eyeballs(addrs: impl IntoIterator<Item = SocketAddr>) -> io::Result<Connection> {
+    let mut attempts = FuturesUnordered::new();
+    let mut current_attempt_id = 0u64;
 
-async fn attempt(
-    id: u64,
-    prev_fail_receiver: Option<oneshot::Receiver<()>>,
-    mut attempt_sender: mpsc::UnboundedSender<Pin<Box<dyn Future<Output = Result<u64, u64>>>>>,
-    t0: Instant,
-) -> Result<u64, u64> {
-    if let Some(prev_fail) = prev_fail_receiver {
-        // Timeout isn't an error here, so ignore the result.
-        let _ = prev_fail.timeout(Duration::from_millis(WAIT_MILLIS)).await;
-    }
-    log(id, "started", t0);
-    let (fail_sender, fail_receiver) = oneshot::channel();
-    if id + 1 < MAX_ATTEMPTS {
-        attempt_sender
-            .send(Box::pin(attempt(
-                id + 1,
-                Some(fail_receiver),
-                attempt_sender.clone(),
-                t0,
-            )))
-            .await
-            .unwrap();
-    } else {
-        eprintln!("Reached the maximum number of attempts.");
-    }
-    match dummy_dns_lookup().await {
-        Ok(()) => Ok(id),
-        Err(()) => {
-            // Ignore failure in this send, because the receiving attempt might
-            // have failed already too.
-            let _ = fail_sender.send(());
-            Err(id)
-        }
-    }
-}
+    // For each addr in the iterator, create a future to connect to it, and add
+    // that future to the attempts collection. Then wait for any of the
+    // following:
+    //   1. any attempt to succeed
+    //   2. the most recent attempt to fail
+    //   3. the WAIT_MILLIS timeout to pass
+    // In the first case, return the successful connection. In the other cases,
+    // continue looping to the next addr.
+    for addr in addrs {
+        current_attempt_id += 1;
+        attempts.push(async move {
+            let result = dummy_connect(addr).await;
+            (current_attempt_id, result)
+        });
+        let timeout = task::sleep(Duration::from_millis(WAIT_MILLIS)).fuse();
+        pin_mut!(timeout);
 
-async fn happy_eyeballs() -> Result<(), ()> {
-    let t0 = Instant::now();
-    let (mut nursery, mut attempt_sender) = Nursery::new();
-    attempt_sender
-        .send(Box::pin(attempt(0, None, attempt_sender.clone(), t0)))
-        .await
-        .unwrap();
-    drop(attempt_sender);
-    loop {
-        match nursery.next().await {
-            Some(Ok(id)) => {
-                log(id, "succeeded", t0);
-                return Ok(());
-            }
-            Some(Err(id)) => {
-                log(id, "failed", t0);
-            }
-            None => {
-                eprintln!("All attempts failed.");
-                return Err(());
+        // Several attempts might fail while we wait for the one we just
+        // spawned, so we select in a loop.
+        loop {
+            select! {
+                _ = timeout => break,
+                attempt = attempts.next() => {
+                    let (id, result) = attempt.expect("at least one attempt in flight");
+                    match result {
+                        Ok(conn) => return Ok(conn),
+                        Err(_) => {
+                            // If the failure was from any job besides the
+                            // current one, continue this wait loop.
+                            if id == current_attempt_id {
+                                break;
+                            }
+                        },
+                    }
+                }
             }
         }
     }
+
+    // At this point we've spawned all the attempts we're going to spawn. Await
+    // any remaining attempts that haven't already failed.
+    while let Some((_, result)) = attempts.next().await {
+        if let Ok(conn) = result {
+            return Ok(conn);
+        }
+    }
+
+    Err(io::Error::new(
+        io::ErrorKind::ConnectionAborted,
+        "all addresses failed to connect",
+    ))
 }
 
-#[tokio::main]
-async fn main() {
-    let _ = happy_eyeballs().await;
+fn main() {
+    // Initialize the START time first thing.
+    let _ = *START;
+    let addrs = vec![
+        SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 80)),
+        SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 2), 80)),
+        SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 3), 80)),
+    ];
+    match task::block_on(happy_eyeballs(addrs)) {
+        Ok(_) => eprintln!("Connected!"),
+        Err(_) => eprintln!("Failed!"),
+    }
 }
