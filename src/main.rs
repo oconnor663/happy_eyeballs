3ff9e8960d3765a9eec78522be1491fd73b170b1
Jack O'Connor <oconnor663@gmail.com>
2019-08-28 10:31:47 -0400

    a recursive implementation with Canceler

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,111 @@
 use async_std::io;
 use async_std::task;
+use futures::channel::{mpsc, oneshot};
+use futures::future::FusedFuture;
 use futures::pin_mut;
 use futures::prelude::*;
 use futures::select;
-use futures::stream::FuturesUnordered;
+use lazy_static::lazy_static;
 use rand::prelude::*;
 use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4};
 use std::time::{Duration, Instant};
-use lazy_static::lazy_static;
 
 const WAIT_MILLIS: u64 = 250;
 
-type Connection = ();
-
 lazy_static! {
     static ref START: Instant = Instant::now();
+}
+
+struct Canceler {
+    drop_sender: Option<oneshot::Sender<()>>,
+    cleanup_handle: task::JoinHandle<()>,
+}
+
+impl Canceler {
+    fn new() -> (Canceler, CancelSpawner) {
+        let (task_sender, mut task_receiver) = mpsc::unbounded();
+        let spawner = CancelSpawner { task_sender };
+        let (drop_sender, drop_receiver) = oneshot::channel();
+        let cleanup_handle = task::spawn(async move {
+            let mut handles = Vec::new();
+            let mut cancel_senders = Vec::new();
+            let mut fused_drop_receiver = drop_receiver.fuse();
+            loop {
+                select! {
+                    pair = task_receiver.next() => {
+                        if let Some((handle, cancel_sender)) = pair {
+                            handles.push(handle);
+                            cancel_senders.push(cancel_sender);
+                        } else {
+                            break;
+                        }
+                    }
+                    _ = fused_drop_receiver => {
+                        break;
+                    }
+                }
+            }
+            if !fused_drop_receiver.is_terminated() {
+                let _ = fused_drop_receiver.await;
+            }
+            for cancel_sender in cancel_senders {
+                let _ = cancel_sender.send(());
+            }
+            for handle in handles {
+                handle.await;
+            }
+        });
+        (
+            Canceler {
+                drop_sender: Some(drop_sender),
+                cleanup_handle,
+            },
+            spawner,
+        )
+    }
+}
+
+impl Drop for Canceler {
+    fn drop(&mut self) {
+        self.drop_sender.take().unwrap().send(()).expect("Canceller::drop");
+        task::block_on(&mut self.cleanup_handle);
+    }
+}
+
+#[derive(Clone)]
+struct CancelSpawner {
+    task_sender: mpsc::UnboundedSender<(task::JoinHandle<()>, oneshot::Sender<()>)>,
+}
+
+impl CancelSpawner {
+    fn spawn<F>(&mut self, future: F)
+    where
+        F: Future + Send + 'static,
+    {
+        // If the receiver is already gone, then cancellation has already
+        // happened, and we shouldn't spawn this task. This check is racy,
+        // however, so we also have to handle send failure below.
+        if self.task_sender.is_closed() {
+            return;
+        }
+        let (cancel_sender, cancel_receiver) = oneshot::channel::<()>();
+        let handle = task::spawn(async move {
+            pin_mut!(future);
+            select! {
+                _ = future.fuse() => {}
+                _ = cancel_receiver.fuse() => {}
+            }
+        });
+        let send_result = self.task_sender.unbounded_send((handle, cancel_sender));
+        // Even though we checked to see whether the channel was closed at the
+        // top, it might've closed in the meantime, and sending to it might
+        // fail here. In this case, cancel the task immediately.
+        if let Err(e) = send_result {
+            let (handle, cancel_sender) = e.into_inner();
+            let _ = cancel_sender.send(());
+            task::block_on(handle);
+        }
+    }
 }
 
 fn log(addr: std::net::SocketAddr, verb: &str) {
@@ -25,6 +116,8 @@ fn log(addr: std::net::SocketAddr, verb: &str) {
     };
     eprintln!("{} {} at {} ms", last_digit, verb, millis);
 }
+
+type Connection = ();
 
 // Simulate a "DNS lookup" (really any kind of network IO, who cares what) by
 // just waiting for some random number of milliseconds, flipping a coin, and
@@ -46,61 +139,74 @@ async fn dummy_connect(addr: std::net::SocketAddr) -> io::Result<Connection> {
     }
 }
 
-async fn happy_eyeballs(addrs: impl IntoIterator<Item = SocketAddr>) -> io::Result<Connection> {
-    let mut attempts = FuturesUnordered::new();
-    let mut current_attempt_id = 0u64;
-
-    // For each addr in the iterator, create a future to connect to it, and add
-    // that future to the attempts collection. Then wait for any of the
-    // following:
-    //   1. any attempt to succeed
-    //   2. the most recent attempt to fail
-    //   3. the WAIT_MILLIS timeout to pass
-    // In the first case, return the successful connection. In the other cases,
-    // continue looping to the next addr.
-    for addr in addrs {
-        current_attempt_id += 1;
-        attempts.push(async move {
-            let result = dummy_connect(addr).await;
-            (current_attempt_id, result)
-        });
-        let timeout = task::sleep(Duration::from_millis(WAIT_MILLIS)).fuse();
-        pin_mut!(timeout);
-
-        // Several attempts might fail while we wait for the one we just
-        // spawned, so we select in a loop.
-        loop {
+fn attempt(
+    mut addrs: impl Iterator<Item = SocketAddr> + Send + 'static,
+    mut spawner: CancelSpawner,
+    prev_failed: Option<oneshot::Receiver<()>>,
+    mut success: mpsc::Sender<Connection>,
+) -> impl Future<Output = ()> + Send + 'static {
+    async move {
+        let addr = if let Some(addr) = addrs.next() {
+            addr
+        } else {
+            return;
+        };
+        if let Some(prev_failed) = prev_failed {
+            let timeout = task::sleep(Duration::from_millis(WAIT_MILLIS));
+            pin_mut!(timeout);
             select! {
-                _ = timeout => break,
-                attempt = attempts.next() => {
-                    let (id, result) = attempt.expect("at least one attempt in flight");
-                    match result {
-                        Ok(conn) => return Ok(conn),
-                        Err(_) => {
-                            // If the failure was from any job besides the
-                            // current one, continue this wait loop.
-                            if id == current_attempt_id {
-                                break;
-                            }
-                        },
+                _ = timeout.fuse() => {}
+                result = prev_failed.fuse() => {
+                    // Short-circuit if the failure sender was dropped, because
+                    // that means the previous attempt succeeded.
+                    if let Err(oneshot::Canceled) = result {
+                        return;
                     }
                 }
             }
         }
-    }
-
-    // At this point we've spawned all the attempts we're going to spawn. Await
-    // any remaining attempts that haven't already failed.
-    while let Some((_, result)) = attempts.next().await {
-        if let Ok(conn) = result {
-            return Ok(conn);
+        let (fail_sender, fail_receiver) = oneshot::channel();
+        spawner.spawn(attempt(
+            addrs,
+            spawner.clone(),
+            Some(fail_receiver),
+            success.clone(),
+        ));
+        match dummy_connect(addr).await {
+            Ok(conn) => {
+                let _ = success.send(conn).await;
+            }
+            Err(_) => {
+                let _ = fail_sender.send(());
+            }
         }
     }
+}
 
-    Err(io::Error::new(
-        io::ErrorKind::ConnectionAborted,
-        "all addresses failed to connect",
-    ))
+async fn happy_eyeballs<A, I>(addrs: A) -> io::Result<Connection>
+where
+    A: IntoIterator<Item = SocketAddr, IntoIter = I>,
+    I: Iterator<Item = SocketAddr> + Send + 'static,
+{
+    let (_canceler, mut spawner) = Canceler::new();
+    let (success_sender, mut success_receiver) = mpsc::channel(0);
+    spawner.spawn(attempt(
+        addrs.into_iter(),
+        spawner.clone(),
+        None,
+        success_sender,
+    ));
+
+    if let Some(conn) = success_receiver.next().await {
+        Ok(conn)
+    } else {
+        Err(io::Error::new(
+            io::ErrorKind::ConnectionAborted,
+            "all addresses failed to connect",
+        ))
+    }
+
+    // _canceler implicitly dropped
 }
 
 fn main() {
